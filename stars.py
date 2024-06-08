468358113c57204d5d3ae9d85fd4ef15908ed7a7
jorden17 <147804450+jorden17@users.noreply.github.com>
2024-06-08 21:51:05 +0530

    Add files via upload

---
+++ stars.py
@@ -0,0 +1,21 @@
+def lower_triangle(height):
+    for i in range(1, height + 1):
+        print('* ' * i)
+
+def upper_triangle(height):
+    for i in range(height, 0, -1):
+        print('* ' * i)
+
+def pyramid_triangle(height):
+    for i in range(1, height + 1):
+        print(' ' * (height - i) + '* ' * i)
+
+# Example usage:
+print("Lower Triangle:")
+lower_triangle(5)
+
+print("\nUpper Triangle:")
+upper_triangle(5)
+
+print("\nPyramid Triangle:")
+pyramid_triangle(5)
